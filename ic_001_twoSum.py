4d48abc531253c15723081a170a6a3af868731ce
Leanna Morinishi <leanna.morinishi@gmail.com>
2019-05-07 10:14:38 -0700

    removed old files

--- ic_001_twoSum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        diff_dict = {}
-        for i in range(len(nums)):
-            if nums[i] in diff_dict:
-                return [i, diff_dict[nums[i]]]
-            else:
-                diff_dict[target - nums[i]] = i
-        return
